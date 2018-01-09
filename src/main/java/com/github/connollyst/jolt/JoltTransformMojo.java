4b9b5c92280895c92a9528a3ce1605b59476e3ed
Sean Connolly <connolly.st@gmail.com>
2018-01-10 09:06:17 +1000

    Switching from input/output files to whole directories.

--- src/main/java/com/github/connollyst/jolt/JoltTransformMojo.java
+++ src/main/java/com/github/connollyst/jolt/JoltTransformMojo.java
@@ -7,9 +7,10 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -23,21 +24,28 @@ public class JoltTransformMojo extends AbstractMojo {
     private String source;
     @Parameter(property = "spec")
     private String spec;
-    @Parameter(property = "result", defaultValue = "/target/jolt-result.json")
-    private String result;
     @Parameter(property = "minify", defaultValue = "false")
     private boolean minify;
 
-    @Parameter(defaultValue = "${project}", readonly = true, required = true)
-    private MavenProject project;
+    @Parameter(property = "transform.inputDirectory", defaultValue = "${project.basedir}/src/main/resources/json/")
+    private String inputDirectory;
+
+    @Parameter(property = "transform.outputDirectory", defaultValue = "${project.build.directory}/json/")
+    private String outputDirectory;
 
     private final ObjectMapper mapper = new ObjectMapper();
 
     public void execute() throws MojoExecutionException, MojoFailureException {
-        Path specFile = asInputFile(spec);
-        Path sourceFile = asInputFile(source);
-        Path resultFile = asOutputFile(result);
-        new JoltTransformer(getLog(), mapper, getWriter(), specFile, resultFile).execute(sourceFile);
+        JoltTransformer transformer = new JoltTransformer(getLog(), mapper, getWriter(), asPath(spec), asPath(outputDirectory));
+        try {
+
+            for (file in Files.list(asPath(inputDirectory))) {
+                Path sourceFile = asInputFile(source);
+                transformer.execute(sourceFile);
+            }
+        } catch (IOException ioe) {
+
+        }
     }
 
     private ObjectWriter getWriter() {
@@ -56,28 +64,12 @@ public class JoltTransformMojo extends AbstractMojo {
         }
     }
 
-    private Path asOutputFile(String filePath) throws MojoExecutionException {
+    private Path asPath(String filePath) throws MojoExecutionException {
         try {
             return Paths.get(getClass().getResource(filePath).toURI());
         } catch (URISyntaxException e) {
             throw new MojoExecutionException("", e);
         }
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public String getSpec() {
-        return spec;
-    }
-
-    public String getResult() {
-        return result;
-    }
-
-    public void setMinify(boolean minify) {
-        this.minify = minify;
     }
 
 }
